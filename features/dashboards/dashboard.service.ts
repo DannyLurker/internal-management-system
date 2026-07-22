cf46b1d37214c512dcd1fde43b7d36c55c7ce8ef
responsainveniree <responsainveniree@gmail.com>
2026-07-22 11:06:00 +0700

    feat: added managerGetDashboard logic

--- features/dashboards/dashboard.service.ts
+++ features/dashboards/dashboard.service.ts
@@ -1,19 +1,75 @@
 import { ManagerDashboardParamSchema } from "@/shared/lib/zods/dashboard.zod";
 import { Prisma, PrismaClient } from "@prisma/client";
 import { Session } from "next-auth";
-import stockMovementsRepository from "../stock-movements/stock-movements.repository";
+import stockMovementsRepository, {
+  createStockMovementWhereInput,
+} from "../stock-movements/stock-movements.repository";
 
 const dashboardService = {
   // Manager Dashboard
-  // managerGetDashboard: async (
-  //   session: Session["user"],
-  //   params: ManagerDashboardParamSchema,
-  //   prisma: Prisma.TransactionClient | PrismaClient,
-  // ) => {
-  //   const [totalInventoryValue, totalSpend, totalStockWastageValue] =  Promise.all([
-  //       await stockMovementsRepository.
-  //   ])
-  // },
+  managerGetDashboard: async (
+    session: Session["user"],
+    params: ManagerDashboardParamSchema,
+    prisma: Prisma.TransactionClient | PrismaClient,
+  ) => {
+    const totalSpendWhereInput = createStockMovementWhereInput({
+      type: "RECEIVE",
+    });
+
+    const totalWastageValueWhereInput = createStockMovementWhereInput({
+      type: {
+        in: ["DISCARD", "MARK_AS_DAMAGED", "MARK_AS_EXPIRED", "MARK_AS_LOST"],
+      },
+    });
+
+    const [totalSpend, totalStockWastageValue] = await Promise.all([
+      stockMovementsRepository.calculateInventoryValue(
+        totalSpendWhereInput,
+        prisma,
+      ),
+      stockMovementsRepository.calculateInventoryValue(
+        totalWastageValueWhereInput,
+        prisma,
+      ),
+    ]);
+
+    const totalInventoryValue =
+      (totalSpend ?? 0) - (totalStockWastageValue ?? 0);
+
+    const lowStockAlertPage =
+      (params.lowStockAlertPagination - 1) * params.lowStockAlertDataPerPage;
+
+    const lowStockAlertDataPerPage = params.lowStockAlertDataPerPage;
+
+    const lowStocks = prisma.$queryRaw`
+    SELECT
+      i."id",
+      i."name",
+      i."minThreshold",
+      i."isActive",
+      COALESCE(SUM(s."quantity"), 0) AS "currentStock"
+    FROM "Item" i
+    LEFT JOIN "Stock" s
+      ON s."itemId" = i."id"
+      AND s."type" = 'READY'
+    WHERE i."isActive" = true
+    GROUP BY
+      i."id",
+      i."name",
+      i."minThreshold",
+      i."isActive"
+    HAVING COALESCE(SUM(s."quantity"), 0) <= i."minThreshold"
+    LIMIT ${lowStockAlertPage}
+    OFFSET ${lowStockAlertDataPerPage};
+    `;
+
+    return {
+      totalSpend,
+      totalInventoryValue,
+      totalStockWastageValue,
+      lowStocks,
+    };
+  },
 };
 
 export default dashboardService;
