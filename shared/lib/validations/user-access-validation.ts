3a6523961f8b73a31c9f3c57ee5edc6fdc092f1c
responsainveniree <responsainveniree@gmail.com>
2026-08-24 12:23:39 +0700

    feat: add stock request type and start working on the stock request logic

--- shared/lib/validations/user-access-validation.ts
+++ shared/lib/validations/user-access-validation.ts
@@ -1,64 +1,69 @@
-import { Roles } from "@prisma/client";
+import { Role } from "@prisma/client";
 
 const PERMISSIONS = {
-  MANAGE_LOCATION: ["HOTEL_MANAGER", "SUPERVISOR"] as Roles[],
-  MANAGE_CATEGORY: ["HOTEL_MANAGER", "SUPERVISOR"] as Roles[],
-  MANAGE_ITEM: ["HOTEL_MANAGER", "SUPERVISOR"] as Roles[],
-  MANAGE_STOCK: ["HOTEL_MANAGER", "SUPERVISOR"] as Roles[],
-  MANAGE_STOCK_MOVEMENT: ["HOTEL_MANAGER", "SUPERVISOR"] as Roles[],
-  MANAGER_LAUNDRY: ["HOTEL_MANAGER", "SUPERVISOR"] as Roles[],
-  ACCESS_MANAGER_DASHBOARD: "HOTEL_MANAGER" as Roles,
-  PRINT_REPORT: ["HOTEL_MANAGER", "ACCOUNTANT"] as Roles[],
+  MANAGE_LOCATION: ["HOTEL_MANAGER", "SUPERVISOR"] as Role[],
+  MANAGE_CATEGORY: ["HOTEL_MANAGER", "SUPERVISOR"] as Role[],
+  MANAGE_ITEM: ["HOTEL_MANAGER", "SUPERVISOR"] as Role[],
+  MANAGE_STOCK: ["HOTEL_MANAGER", "SUPERVISOR"] as Role[],
+  MANAGE_STOCK_MOVEMENT: ["HOTEL_MANAGER", "SUPERVISOR"] as Role[],
+  MANAGER_LAUNDRY: ["HOTEL_MANAGER", "SUPERVISOR"] as Role[],
+  ACCESS_MANAGER_DASHBOARD: "HOTEL_MANAGER" as Role,
+  PRINT_REPORT: ["HOTEL_MANAGER", "ACCOUNTANT"] as Role[],
+  STOCK_REQUEST: ["HOUSEKEEPING", "FRONT_DESK"] as Role[],
 };
 
-export const canManageLocation = (role: Roles) => {
+export const canManageLocation = (role: Role) => {
   return PERMISSIONS.MANAGE_LOCATION.includes(role);
 };
 
-export const canManageCategory = (role: Roles) => {
+export const canManageCategory = (role: Role) => {
   return PERMISSIONS.MANAGE_CATEGORY.includes(role);
 };
 
-export const canManageItem = (role: Roles) => {
+export const canManageItem = (role: Role) => {
   return PERMISSIONS.MANAGE_ITEM.includes(role);
 };
 
-export const canManageStock = (role: Roles) => {
+export const canManageStock = (role: Role) => {
   return PERMISSIONS.MANAGE_STOCK.includes(role);
 };
 
-export const canManageStockMovement = (role: Roles) => {
+export const canManageStockMovement = (role: Role) => {
   return PERMISSIONS.MANAGE_STOCK_MOVEMENT.includes(role);
 };
 
-export const canDeleteLocation = (role: Roles) => {
+export const canDeleteLocation = (role: Role) => {
   return role === "HOTEL_MANAGER";
 };
 
-export const canDeleteCategory = (role: Roles) => {
+export const canDeleteCategory = (role: Role) => {
   return role === "HOTEL_MANAGER";
 };
 
-export const canDeleteItem = (role: Roles) => {
+export const canDeleteItem = (role: Role) => {
   return role === "HOTEL_MANAGER";
 };
 
-export const canDeleteStock = (role: Roles) => {
+export const canDeleteStock = (role: Role) => {
   return role === "HOTEL_MANAGER";
 };
 
-export const canDeleteStockMovement = (role: Roles) => {
+export const canDeleteStockMovement = (role: Role) => {
   return role === "HOTEL_MANAGER";
 };
 
-export const canAccessManagerDashboard = (role: Roles) => {
+export const canAccessManagerDashboard = (role: Role) => {
   return role === PERMISSIONS.ACCESS_MANAGER_DASHBOARD;
 };
 
-export const canManageLaundry = (role: Roles) => {
+export const canManageLaundry = (role: Role) => {
   return PERMISSIONS.MANAGER_LAUNDRY.includes(role);
 };
 
-export const canPrintReport = (role: Roles) => {
+export const canPrintReport = (role: Role) => {
   return PERMISSIONS.PRINT_REPORT.includes(role);
+};
+
+export const canAccessStockRequest = (role: Role) => {
+  return PERMISSIONS.STOCK_REQUEST.includes(role);
 };
